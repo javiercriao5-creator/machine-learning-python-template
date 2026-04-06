b7a5c1cb304ea6139985624eefbcf994f6cc4e8d
Javier Criao <javiercriao5@gmail.com>
2026-04-06 08:49:36 +0000

    Mejora con pandas

--- src/app.py
+++ src/app.py
@@ -2,24 +2,31 @@
 # engine = db_connect()
 
 # your code here
+import pandas as pd
 import numpy as np
 from flask import Flask, request, render_template
 import pickle
 
 app = Flask(__name__)
 
-# Como app.py y wine_model.pkl están en la misma carpeta (src), esto funcionará perfecto
 model = pickle.load(open('wine_model.pkl', 'rb'))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Recopilamos los 11 valores químicos del formulario
+        # 1. Recoger valores
         features = [float(x) for x in request.form.values()]
-        final_features = [np.array(features)]
         
-        # Hacemos la predicción
-        prediction = model.predict(final_features)
+        # 2. Nombres EXACTOS de las columnas del dataset original
+        column_names = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar',
+                        'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density',
+                        'pH', 'sulphates', 'alcohol']
+        
+        # 3. Crear un DataFrame de Pandas (como lo espera el modelo)
+        df_features = pd.DataFrame([features], columns=column_names)
+        
+        # 4. Predecir
+        prediction = model.predict(df_features)
         output = prediction[0]
         
         return render_template('index.html', prediction_text=f'La calidad predicha de este vino es: {output} (escala 0-10)')
